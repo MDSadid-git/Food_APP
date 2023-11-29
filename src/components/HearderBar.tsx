43cc1a65e81977facea0a70b66555b65c528ea93
Sadid <sfhsadid@gmail.com>
2023-11-30 03:36:00 +0600

    first commit

---
+++ src/components/HearderBar.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable prettier/prettier */
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {COLORS, FONTFAMILY, FONTSIZE, SPACING} from '../theme/theme';
+import GradientBGIcon from './GradientBGIcon';
+import ProfilePic from './ProfilePic';
+
+interface HeaderBarProps {
+  title?: string;
+}
+const HearderBar: React.FC<HeaderBarProps> = ({title}) => {
+  return (
+    <View style={styles.HeaderContainer}>
+      <GradientBGIcon
+        name="menu"
+        color={COLORS.primaryPinkHex}
+        size={FONTSIZE.size_16}
+      />
+      <Text style={styles.HeaderText}>{title}</Text>
+      <ProfilePic />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  HeaderContainer: {
+    padding: SPACING.space_30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  HeaderText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_20,
+    color: COLORS.primaryWhiteHex,
+  },
+});
+export default HearderBar;
